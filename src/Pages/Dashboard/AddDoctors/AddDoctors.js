0753e6270d4abf35eb1b63c8cfdcf06e37665336
rakib511 <rakib660299@gmail.com>
2021-11-08 01:10:53 -0800

    first commit

---
+++ src/Pages/Dashboard/AddDoctors/AddDoctors.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddDoctors = () => {
+    return (
+        <div>
+            <h3>add a doctors</h3>
+        </div>
+    );
+};
+
+export default AddDoctors;
---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyACQPth7z9JNuNcJZaZwrAWLL--50ulB_o",
+  authDomain: "doctors-portal-22c67.firebaseapp.com",
+  projectId: "doctors-portal-22c67",
+  storageBucket: "doctors-portal-22c67.appspot.com",
+  messagingSenderId: "1027998208026",
+  appId: "1:1027998208026:web:973b3ac62c70dcac5f41a3"
+};
+export default firebaseConfig;
